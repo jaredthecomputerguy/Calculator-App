a293b6033ec4132eb402d3da39e1109fa0c3578b
Jared Mercer <jaredthebackpacker@gmail.com>
2022-09-19 12:29:43 -0700

    Removed sample JS code, removed % from buttons

--- script.js
+++
@@ -1,7 +0,0 @@
-const allBtns = document.querySelectorAll(".btn");
-
-allBtns.forEach((button) => {
-  button.addEventListener("mousedown", (e) => {
-    button.classList.add("clicked");
-  });
-});
